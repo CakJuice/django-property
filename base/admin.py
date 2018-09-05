2288de1433e2760007d632cf29bf53c69ff1a890
Hendra juice <hd.brandoz@gmail.com>
2018-09-05 20:39:24 +0700

    create base admin & migration

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import SiteSetting
+
+
 # Register your models here.
+class BaseAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        obj.save()
+
+
+@admin.register(SiteSetting)
+class SiteSettingAdmin(BaseAdmin):
+    fields = ('key', 'value')
+    list_display = ('key', 'value')
--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
+    created_by = models.ForeignKey(User, on_delete='RESTRICT', related_name='+', verbose_name="Created By")
+    updated_by = models.ForeignKey(User, on_delete='RESTRICT', related_name='+', verbose_name="Updated By")
+
+    class Meta:
+        abstract = True
+
+
+class SiteSetting(BaseModel):
+    key = models.CharField(max_length=64, unique=True, verbose_name="Key")
+    value = models.CharField(max_length=200, verbose_name="Value", null=True, blank=True)
