63d0dac26ea43e6763699570161fcabf5861f9ec
Hendra juice <hd.brandoz@gmail.com>
2018-09-05 20:13:24 +0700

    update base

--- base/views.py
+++ base/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return HttpResponse('Homepage')
+    return render(request, 'base/homepage.html')
