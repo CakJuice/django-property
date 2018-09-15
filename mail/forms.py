bdedd83dd46c71e638f427710151ae112f7a70fe
Hendra juice <hd.brandoz@gmail.com>
2018-09-15 20:30:27 +0700

    tagsinput mail & remove disabled on templates

--- mail/forms.py
+++ mail/forms.py
@@ -4,6 +4,9 @@ from .models import Mail
 
 
 class MailForm(forms.ModelForm):
+    email_to = forms.CharField(required=True, widget=forms.TextInput(attrs={'data-role': 'tagsinput'}))
+    email_cc = forms.CharField(required=True, widget=forms.TextInput(attrs={'data-role': 'tagsinput'}))
+
     class Meta:
         model = Mail
         fields = ('email_from', 'email_to', 'email_cc', 'subject', 'body', 'attachment', 'state', 'send_at')
